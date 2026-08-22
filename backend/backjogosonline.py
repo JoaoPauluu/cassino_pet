9e7af97a2f9601fdb6af8728806184cde52ff78f
Rodrigo <rodrigo.eberr@gmail.com>
2026-08-21 21:01:23 -0300

    Add files via upload

---
+++ backend/backjogosonline.py
@@ -0,0 +1,34 @@
+import random as rand
+
+def roletaeuropeia():
+    num = rand.randint(0,12)
+
+    red = {x: x % 2 == 1 for x in range(1,12)}
+    black = {x: x % 2 == 0 for x in range(1,12)}
+
+    if num in black:
+        color = "black"
+    elif num in red:
+        color = "red"
+    else:
+        color = "green"
+
+    return {
+        'Resultado': num,
+        'Cor': color
+    }
+
+def crashout():
+    houseedge = 0.2
+
+    if rand.random() < houseedge:
+        return round(rand.uniform(0, houseedge),2)
+    
+    ligma = rand.random()
+    mult = 1 / (1 - ligma)
+
+    maxmult = 20
+    if mult > maxmult:
+        mult = maxmult
+
+    return round(mult, 2)
